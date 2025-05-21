e881302fedfca68f5357193d67e7387792569a78
IsaqueMunizz <isaquemuniz3250@gmail.com>
2025-05-20 21:15:04 -0300

    commit buscaPreOrdem

--- src/No.java
+++ src/No.java
@@ -10,7 +10,12 @@ public class No {
  if (node == null) return 0;
     return 1 + contarNos(node.esquerda) + contarNos(node.direita);
 }
-
-
-
+    public void buscaPreOrdem(No node) {
+        if (node != null) {
+            System.out.print(node.valor + " ");
+            buscaPreOrdem(node.esquerda);
+            buscaPreOrdem(node.direita);
+        }
+    }
 }
+
